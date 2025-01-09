54c12157c6f0ec97e567983146374fd099724226
Flamingo <Flamingo7562@gmail.com>
2025-01-09 17:56:40 +0900

    temp login

--- app.js
+++ app.js
@@ -1,7 +1,24 @@
-const h1 = document.querySelector("div.hello:first-child h1");
+const loginForm = document.querySelector("#login-form");
+const loginInput = loginForm.querySelector("input");
+const link = document.querySelector("a");
+const greeting = document.querySelector("#greeting");
 
-function handleTitleClick() {
-  h1.classList.toggle("clicked");
+const HIDDEN_CLASSNAME = "hidden";
+// const loginInput = document.qeurySelector("#login-form input"); 이런식으로도 작성이 가능하다.
+
+function onLoginSubmit(tomato) {
+  tomato.preventDefault();
+  const username = loginInput.value;
+  loginForm.classList.add(HIDDEN_CLASSNAME);
+  greeting.innerText = `Hello ${username}`;
+  greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
-h1.addEventListener("click", handleTitleClick);
+function handleLinkClick(tomato) {
+  tomato.preventDefault();
+  console.log(tomato);
+  //alert("clicked");
+}
+
+loginForm.addEventListener("submit", onLoginSubmit);
+link.addEventListener("click", handleLinkClick);
