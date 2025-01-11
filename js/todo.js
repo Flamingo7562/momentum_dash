f420752168e6634cbc19aab10315f4cd1cb16c06
Flamingo <Flamingo7562@gmail.com>
2025-01-11 20:54:10 +0900

    Todo List

--- js/todo.js
+++ js/todo.js
@@ -2,10 +2,31 @@ const toDoForm = document.getElementById("todo-form");
 const toDoInput = toDoForm.querySelector("input");
 const toDoList = document.getElementById("todo-list");
 
+function deleteToDO(tomato) {
+  const potato = tomato.target.parentElement;
+  potato.remove();
+}
+
+function paintToDO(newTodo) {
+  //console.log(`i will paint ${newTodo}`);
+  const potato = document.createElement("li");
+  const span = document.createElement("span");
+  const btn = document.createElement("button");
+
+  btn.innerText = "❌";
+  btn.addEventListener("click", deleteToDO);
+
+  potato.appendChild(span);
+  potato.appendChild(btn);
+  span.innerText = newTodo;
+  toDoList.appendChild(potato);
+}
+
 function handleToDoSubmit(tomato) {
   tomato.preventDefault();
-  const newToDo = toDoInput.value;
+  const newTodo = toDoInput.value;
   toDoInput.value = "";
+  paintToDO(newTodo);
 }
 
 toDoForm.addEventListener("submit", handleToDoSubmit);
