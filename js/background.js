51d7e123cfb861bd930d6b2230db41373adce7d2
Flamingo <Flamingo7562@gmail.com>
2025-01-12 03:00:22 +0900

    style changed

--- js/background.js
+++ js/background.js
@@ -1,8 +1,6 @@
 const images = ["0.png", "1.png", "2.png", "3.png"];
 const chosenImage = images[Math.floor(Math.random() * images.length)];
-//console.log(chosenImage);
 
-const bgImage = document.createElement("img");
-bgImage.src = `img/${chosenImage}`;
-
-document.body.appendChild(bgImage); 
+//document.body.appendChild(bgImage);
+document.body.style.backgroundImage = `url(img/${chosenImage})`;
+document.body.style.backgroundSize = "cover";
