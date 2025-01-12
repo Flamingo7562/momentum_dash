0b33710c1d6ca289cb3d09c6209da813e817e4bb
Flamingo <Flamingo7562@gmail.com>
2025-01-12 18:27:25 +0900

    new images

--- js/background.js
+++ js/background.js
@@ -1,4 +1,9 @@
-const images = ["0.png", "1.png", "2.png", "3.png"];
+function createArray(count) {
+  return Array.from({ length: count }, (_, i) => `${i}.png`);
+}
+
+//const images = ["0.png", "1.png", "2.png", "3.png"];
+const images = createArray(28);
 const chosenImage = images[Math.floor(Math.random() * images.length)];
 
 //document.body.appendChild(bgImage);
--- naming.py
+++ naming.py
@@ -1,1 +1,29 @@
-print("Hello")
+import os
+
+def rename_images(folder_path):
+    try:
+        if not os.path.exists(folder_path):
+            raise FileNotFoundError(f"folder '{folder_path}' is not found.")
+        image_extensions = (".jpg", ".jpeg", ".png")
+        image_files = [f for f in os.listdir(folder_path) if f.lower().endswith(image_extensions)]
+        image_files.sort()
+
+        if not image_files:
+            print("There is no Image in folder.")
+            return
+        
+        for i, filename in enumerate(image_files):
+            src = os.path.join(folder_path, filename)
+            new_filename = str(i) + os.path.splitext(filename)[1]
+            dst = os.path.join(folder_path, new_filename)
+            os.rename(src, dst)
+            print(f"'{filename}' is replaced to '{new_filename}'.")
+        
+        print("All names are changed.")
+    except FileNotFoundError as e:
+        print(e)
+    except Exception as e:
+        print(f"Error occurred : {e}")
+
+folder_path = "C:/Users/Flamingo/Desktop/Desk/images/temp"
+rename_images(folder_path)
